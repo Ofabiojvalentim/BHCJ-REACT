d051ae2331d8d8c5ccdfb3fcfca0ee97ac4637bc
Ofabiojvalentim <fabiojvalentim@gmail.com>
2023-10-27 23:49:34 -0300

    minor updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className="information"> Ofjv - React Site (no REST API) </div>
             <nav className="navbar navbar-expand-lg navbar-light header-snippet">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
+                    <a className="navbar-brand" href="/">
                         <img src="logo_ofjv.png" className="rounded d-inline-block align-text-top" alt="" width="96" height="62"/>
                     </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
